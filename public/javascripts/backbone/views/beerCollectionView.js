a7ce0877d8eb03de51e61f891a394f5afe4b1fd4
wakelank <wakelank@gmail.com>
2014-07-04 10:34:00 -0400

    Fixing bugs with edit

--- public/javascripts/backbone/views/beerCollectionView.js
+++ public/javascripts/backbone/views/beerCollectionView.js
@@ -3,12 +3,12 @@ Beerapp.Views.BeerCollectionView = Backbone.View.extend({
   initialize: function(){
     this.listenTo(this.collection, 'add', this.render);
   },
-  render: function(){
-    var that = this;
-    this.$el.empty()
-    _.each(this.collection.models, function(beer){
-      var beerView = new Beerapp.Views.BeerView({model: beer});
-      that.$el.prepend(beerView.render().el);
-    })
-  }
+  // render: function(){
+  //   var that = this;
+  //   this.$el.empty()
+  //   _.each(this.collection.models, function(beer){
+  //     var beerView = new Beerapp.Views.BeerView({model: beer});
+  //     that.$el.prepend(beerView.render().el);
+  //   })
+  // }
 });
